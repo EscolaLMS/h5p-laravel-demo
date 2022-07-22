c631ad624b07598c68c402d835be795990cbe12c
Mateusz Wojczal <mateusz@wojczal.com>
2022-07-22 20:18:44 +0200

    force https unless localhost

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (strpos(url()->current(), 'localhost') !== false) {
-            \URL::forceScheme('http');
+            \URL::forceScheme('https');
         }
     }
 }
