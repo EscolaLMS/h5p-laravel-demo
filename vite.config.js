44660a5b8ef89f8ccf37db740b4a6fd1c6a794e0
Mateusz Wojczal <mateusz@wojczal.com>
2022-07-22 20:07:30 +0200

    heroku

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/index.tsx"],
             refresh: true,
-            manifest: false,
         }),
     ],
 });
